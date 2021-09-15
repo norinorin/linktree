114bcdb1fb8db3c2836bb59f524c84b99bbe495a
Norizon <69400842+norinorin@users.noreply.github.com>
2021-09-15 20:45:20 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      keyframes: {
+        "gradient-shift": {
+          "0%": { "background-position": "right" },
+          "100%": { "background-position": "left" }
+        }
+      },
+      animation: {
+        "gradient-shift": "gradient-shift 5s ease alternate infinite"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
